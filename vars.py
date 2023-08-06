b8ae5d2d39aae9d999309ce9ce99a8ab9fee3da9
xNoerPlays <97339279+xNoerPlaysCodes@users.noreply.github.com>
2023-08-06 21:22:20 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,4 +1,4 @@
-TOKEN = "MTEzNzc0NTMwMTIyNzE4ODM2Nw.GZEO03.BEP9O9HQy3aKnNVIjLkXSM83nUWMX8LEQQlNPw"
+TOKEN = "BOT_TOKEN_HERE"
 #^^^^ TOKEN (DO NOT SHARE THIS WITH ANYONE)
 api_key = "Vkk8oi5EJg1Clal7BwS884uPvqjhfIhl"
 url = f"https://api.tenor.com/v1/random?key={api_key}"
