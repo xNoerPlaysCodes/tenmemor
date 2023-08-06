672953d130771d7437dbf228a62965f131c928aa
xNoerPlays <97339279+xNoerPlaysCodes@users.noreply.github.com>
2023-08-06 21:23:18 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,4 +1,4 @@
 TOKEN = "BOT_TOKEN_HERE"
 #^^^^ TOKEN (DO NOT SHARE THIS WITH ANYONE)
-api_key = "Vkk8oi5EJg1Clal7BwS884uPvqjhfIhl"
+api_key = "API_KEY_FROM_GIPHY"
 url = f"https://api.tenor.com/v1/random?key={api_key}"
